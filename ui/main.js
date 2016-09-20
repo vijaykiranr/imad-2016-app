e5434e31b017f44c813d1fd97bf59f9c1423226b
vijaykiranr <vijaykiranmepco@gmail.com>
2016-09-20 19:22:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+alert("WELCOME TO MY WORLD!!");
+var y = prompt("Are you READY TO CHANGE THE WORLD FOR GOOD???");
+if(y === true){console.log("To lead a happy life, tie your life to a GOAL, not to PEOPLE or OBJECTS!!!");
+document.writeln("To lead a happy life, tie your life to a GOAL, not to PEOPLE or OBJECTS!!!");
+}
