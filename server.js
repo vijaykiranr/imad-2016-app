f364dde2dd76c36aebe802d3ad6595e1ad9ff3b9
vijaykiranr <vijaykiranmepco@gmail.com>
2016-09-22 03:38:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article-one', function(req,res){
 });
 
 app.get('/article-two', function(req,res){
-   res.send('CONTENT FOR ARTICLE TWO'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req,res){
-    res.send('CONATEANT FOR ARTICLE THREE');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
