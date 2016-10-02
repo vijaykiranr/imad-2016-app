854ed3d430a3356d81c12cb3cd6b1a7d2694d0c1
vijaykiranr <vijaykiranmepco@gmail.com>
2016-10-03 02:09:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,28 @@
-var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE)
+      if(request.status === 200){
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+  };
+};
+
+request.open('GET', "http://vijaykiranr.imad.hasura-app.io/counter", true);
+reuest.send(null);
+
+
+/*var counter = 0;
             var button = document.getElementById('counter');
             button.onclick = function(){
                 var span = document.getElementById('count');
                 counter = counter+1;
                 span.innerHTML = counter.toString();
             };
-
+*/
 /*console.log('Loaded!');
 alert("WELCOME TO MY WORLD!!");
 
