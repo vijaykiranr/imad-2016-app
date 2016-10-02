2dd2a72c474eec0c77b327d3066f0289c34f2d10
vijaykiranr <vijaykiranmepco@gmail.com>
2016-10-02 09:48:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ var y = prompt("Are you READY TO CHANGE THE WORLD FOR GOOD???");
 if(y === true){console.log("To lead a happy life, tie your life to a GOAL, not to PEOPLE or OBJECTS!!!");
 }
 
-var element = getElementById("testid");
+var element = document.getElementById("testid");
 console.log(element);
 element.innerHTML = "THIS IS CHANGED INNER";
