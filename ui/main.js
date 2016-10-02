9f928e1ae2fb88d32845f49f726fde304a7182c2
vijaykiranr <vijaykiranmepco@gmail.com>
2016-10-02 10:54:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,6 @@ element.innerHTML = "";
 element.innerHTML = "<p>PARAGRAPH</p>";
 
 var vj = document.getElementById("vj");
-vj.onMouseDown = function(){
+vj.onClick = function(){
     vj.style.marginLeft = "100px"; 
 };
