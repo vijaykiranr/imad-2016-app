c7c83e87d3d7a55451cdf8923cb309574ae463b1
Vijay Kiran R <vijaykiranmepco@gmail.com>
2016-10-05 07:49:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/article-one', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/dooTest', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'dooTest.html')); 
+});
+
 app.get('/article-two', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
