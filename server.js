4122ff31a99e56d18f9df92cd38ac566ff88e611
Vijay Kiran R <vijaykiranmepco@gmail.com>
2016-10-15 22:26:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('ui/youtubeSearch', function(req,res){
+app.get('/ui/youtubeSearch', function(req,res){
     res.sendFile(path.join(__dirname,'ui','youtubeSearch.html'));
 });
 
-app.get('ui/youtubeScript', function(req,res){
+app.get('/ui/youtubeScript', function(req,res){
    res.sendFile(path.join(__dirname,'ui','youtubeScript.js')); 
 });
 
