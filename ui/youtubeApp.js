b67fcc695c662eb0af5f9555309f3912ae875a3a
Vijay Kiran R <vijaykiranmepco@gmail.com>
2016-10-17 18:36:34 +0530

    [imad-console] Updates ui/youtubeApp.js

--- ui/youtubeApp.js
+++ ui/youtubeApp.js
@@ -1,1 +1,14 @@
-
+var channelName = 'RVK Entertainment'; 
+$(document).ready(function(){
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels",{
+            part:'contentDetails',
+            forUserName:channelName,
+            key:'AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE'},
+            function(data){
+                $.each(data.items,function(){
+                    console.log(item);
+                });
+            }
+        );
+        });
